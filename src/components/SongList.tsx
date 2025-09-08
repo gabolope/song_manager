356ffaf22211e0657f68a9c823404daa5c736bd4
Gabriel Alejandro López <gabolope1517@gmail.com>
2025-09-08 16:34:06 -0300

    Add 'Agregar a lista' button to selected song
    Updated SongList component to display an 'Agregar a lista' button next to the currently selected song. Refactored the list item layout to use Bootstrap grid classes for better alignment.

--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -12,19 +12,28 @@ const SongList = ({ items, selectedSong = 0, onClick }: Props) => {
     <>
       <div className="list-group songListContainer">
         {items.map((item, index) => (
-          <a
-            href="#"
-            aria-current="true"
-            key={index}
-            className={
-              selectedSong === index
-                ? "list-group-item list-group-item-action active"
-                : "list-group-item list-group-item-action"
-            }
-            onClick={() => onClick(index)}
-          >
-            {item.title}
-          </a>
+          <>
+            <div className="row justify-content-between">
+                <a
+                  href="#"
+                  aria-current="true"
+                  key={index}
+                  className={
+                    selectedSong === index
+                      ? "list-group-item list-group-item-action active"
+                      : "list-group-item list-group-item-action"
+                  }
+                  onClick={() => onClick(index)}
+                >
+                  <div className="col-4">
+                    {item.title}
+                  </div>
+                  <div className="col-4">
+                    {selectedSong === index ? <button className="btn btn-outline-light addButton">Agregar a lista</button> :null}
+                  </div>
+                </a>
+            </div>
+          </>
         ))}
       </div>
     </>
