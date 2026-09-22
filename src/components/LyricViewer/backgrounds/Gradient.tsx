8234c8f1deebfd2d14d1264d6787abccb4b56493
Gabriel Alejandro López <78502447+gabolope@users.noreply.github.com>
2026-09-22 16:25:33 -0300

    add gradient

--- src/components/LyricViewer/backgrounds/Gradient.tsx
+++ src/components/LyricViewer/backgrounds/Gradient.tsx
@@ -1,22 +1,8 @@
-/* html, body {
-  width: 100%;
-  height:100%;
-}
+import "./Gradient.css";
 
-body {
-    background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
-    background-size: 400% 400%;
-    animation: gradient 15s ease infinite;
-}
+export const label = "Degradado";
+export const usesColor = false;
 
-@keyframes gradient {
-    0% {
-        background-position: 0% 50%;
-    }
-    50% {
-        background-position: 100% 50%;
-    }
-    100% {
-        background-position: 0% 50%;
-    }
-} */
+const Gradient = () => <div className="gradient-bg" />;
+
+export default Gradient;
